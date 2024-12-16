bdac84a25d0ed6be02474846bab52527a58f05ac
neivanny1 <nevileneivanny@gmail.com>
2024-12-16 13:54:14 +0000

    added function to handle campaign creation

--- contracts/CharityDonation.sol
+++ contracts/CharityDonation.sol
@@ -1,9 +1,66 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.0;
 
 contract CharityDonationPlatform {
     /* VARIABLES AND MAPPINGS ARE DECLARED HERE */
-    /* CONSTRUCTORS ARE DECLARED HERE */
+    // struct to hold compaign data
+    struct Campaign {
+        uint256 id;
+        string title;
+        string description;
+        uint256 targetAmount;
+        uint256 raisedAmount;
+        address payable owner;
+        bool isCompleted;
+    }
+    // Struct to hold donor details
+    struct Donor {
+        address donorAddress;
+        uint256 amount;
+    }
+    // variables
+    uint256 public campaignCount;
+    mapping(uint256 => Campaign) public campaigns;
+    mapping (uint256 => Donor[]) public donations;
+
+    /* EVENTS ARE DECLARED HERE */
+    event CampaignCreated(uint256 campaignId, string title, address indexed owner);
+    event DonationReceived(uint campaignId, address indexed donor, uint256 amount);
+    event FundsWithdrawn(uint campaignId, uint256 amount);
+
+    /* CONSTRUCTORS AND MODIFIERS ARE DECLARED HERE */
+    // modifier to check onwership
+    modifier onlyOnwer(uint256 campaignId) {
+        require(msg.sender == campaigns[campaignId].owner, "You are not the campaign onwer");
+        _;
+    }
+
+    // modifier to check if campaign exists
+    modifier campaignExists(uint256 campaignId) {
+        require(campaignId  < campaignCount, "The campaign doesn`t exist!");
+        _;
+    }
+
     /* FUNCTIONS */
+    // Function to create a campaign
+    function createCampaign(
+        string memory _title,
+        string memory _description,
+        uint256 _targetAmount 
+    ) public {
+        campaigns[campaignCount] = Campaign({
+            id: campaignCount,
+            title: _title,
+            description: _description,
+            targetAmount: _targetAmount,
+            raisedAmount: 0,
+            owner: payable(msg.sender),
+            isCompleted: false
+        });
+        emit CampaignCreated(campaignCount, _title, msg.sender);
+        campaignCount++;
+    }
+    function donateToCampaign() public {}
+    function withdrawFunds() public {}
 
 }
