6e3d4fa27ed1498e72c351f07c6049859432bd10
neivanny1 <nevileneivanny@gmail.com>
2024-12-19 13:45:52 +0000

    added deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,14 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const CharityDonationPlatform = await ethers.getContractFactory("CharityDonationPlatform");
-  const charity = await CharityDonationPlatform.deploy();
-  await charity.deployed();
+    const CharityDonationPlatform = await ethers.getContractFactory("CharityDonationPlatform");
+    const charity = await CharityDonationPlatform.deploy();
+    await charity.deployed();
 
-  console.log("CharityDonationPlatform deployed to:", charity.address);
+    console.log("CharityDonationPlatform deployed to:", charity.address);
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
